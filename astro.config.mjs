e57ec920e0c6af0a79b1125e972b216edbb65036
popeye-ux <popeye.ux@gmail.com>
2026-02-12 01:40:36 +0800

    修改post-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://popeye-ux.github.io",
+    base: "/moments-of-beings",
+});
